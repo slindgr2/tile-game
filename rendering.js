5db1d76e087cf87cb9596f6a38d0a7adadeeaab5
slindgr2 <slindgr2@nebrwesleyan.edu>
2014-11-25 09:19:11 -0600

    Update rendering.js

--- rendering.js
+++ rendering.js
@@ -88,7 +88,7 @@ render();
 
 // Panning the camera
 
-var maximum_pan_rate = 0.03;
+var maximum_pan_rate = 0.01;
 function pan() {
     var dx = protagonist.x - center_x;
     var dy = protagonist.y - center_y;
