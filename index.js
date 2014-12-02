5ead8024e6eafaf2e6e078fe9dfeda8d56063a4c
slindgr2 <slindgr2@nebrwesleyan.edu>
2014-12-02 08:59:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ document.addEventListener('keydown', key_pressed_down);
 
 function enemy_movement(){
     occupants[antagonist.x][antagonist.y] = undefined;
-if (is_in_bounds(antagonist.x,antagonist.y)){
+if (is_in_bounds(antagonist.x + 1,antagonist.y)){
     antagonist.x = antagonist.x + 1;
     }
 }
