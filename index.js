c98e2fc6c80b2977773daf2c1b20b92cb3d1123f
slindgr2 <slindgr2@nebrwesleyan.edu>
2014-12-04 09:23:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -65,10 +65,10 @@ function enemy_movement(event) {
 function respawn_on_collision(event){
     if (protagonist.y === antagonist.y){
         if (protagonist.x === antagonist.x){
-        occupants[protagonist.y][protagonist.x] = undefined;
-        protagonist.y = 7;
-        protagonist.x = 7;
-        occupants[protagonist.y][protagonist.x] = protagonist.elemtent;
+            occupants[protagonist.y][protagonist.x] = undefined;
+            protagonist.y = 7;
+            protagonist.x = 7;
+            occupants[protagonist.y][protagonist.x] = protagonist.elemtent;
     }
 }
     
