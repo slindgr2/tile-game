7fe67a7b7a16c53029ba403834b566e151c7af33
slindgr2 <slindgr2@nebrwesleyan.edu>
2014-12-11 14:36:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ function number_of_lives(){
  l = l - 1;
  document.getElementById("lives").innerHTML = 'Lives: '+l;
  if (l === 0){
-     alert('Game Over! You Suck!\nScore: '+s);
+     alert('Game Over!\nScore: '+s);
  }
  }
 
